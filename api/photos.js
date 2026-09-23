dead8856168838168b5e734c7260d5fb8618c70a
Metadel95 <metadelteshome1995@gmail.com>
2026-09-23 23:28:34 +0300

    Update photos.js

--- api/photos.js
+++ api/photos.js
@@ -15,7 +15,7 @@ module.exports = async function handler(req, res) {
 
   try {
     const result = await cloudinary.search
-      .expression('folder:wedding-sarah-james-2025 AND tags=guest-upload')
+      .expression('folder:wedding-Mekbeb-Nebyat-2025 AND tags=guest-upload')
       .sort_by('created_at', 'desc')
       .with_field('context')
       .max_results(500)
